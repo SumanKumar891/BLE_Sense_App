a72cf8fdc928c9925b9cc6cdaeab8f922931bd2d
Yash Aggarwal <yash2821aggarwal@gmail.com>
2024-12-10 10:37:39 +0530

    metal Detector

---
+++ app/src/main/java/com/merge/awadh/test3_2.kt
@@ -0,0 +1,11 @@
+package com.merge.awadh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class test3_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test32)
+    }
+}
